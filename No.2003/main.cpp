395b289606ceba1911193e4811e479943f1ac11d
Jang Hyun Cho <69357083+cho-jang-hyun@users.noreply.github.com>
2023-08-05 22:45:03 +0900

    main.cpp

---
+++ No.2003/main.cpp
@@ -0,0 +1,47 @@
+// baekjoon 1920
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+// codes for fast I/O
+/*void init(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+}*/
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int input;
+    cin >> input;
+    
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    int count = 0;
+    
+    for(int j = 0; j < n; j++){
+        int sum = 0;
+        
+        for(int i = j; i < n; i++){
+            sum += arr[i];
+            if (sum == input){
+               count++;
+               break;
+            }
+            else if(sum > input){
+                break;
+            }
+            
+        }
+    }
+    cout << count;
+}
+    
+
